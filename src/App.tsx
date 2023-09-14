28819d5ba562efca0f92254bbfae2ddefdc3e4cb
vbuarque <viniciusbuarque17@gmail.com>
2023-09-14 16:29:31 -0300

    feat: criando funções para ler o audio do video

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Github, FileVideo, Upload, Wand2 } from 'lucide-react'
+import { Github, Wand2 } from 'lucide-react'
 import { Button } from "./components/ui/button";
 import { Separator } from './components/ui/separator';
 import { Textarea } from './components/ui/textarea';
 import { Label } from './components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
 import { Slider } from './components/ui/slider';
+import { VideoInputForm } from './components/video-input-form';
 
 export function App() {
   return (
@@ -17,15 +18,15 @@ export function App() {
             Desenvolvido com 💜 no NLW da RocketSeat - por Vinicius Buarque
           </span>
 
-          <Separator orientation='vertical' className="h-6"/>
+          <Separator orientation='vertical' className="h-6" />
 
           <Button variant="outline">
-            <Github className="w-4 h-4 mr-2"/>
+            <Github className="w-4 h-4 mr-2" />
             Github
           </Button>
         </div>
       </div>
-      
+
       <main className="flex-1 p-6 flex gap-6 ">
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
@@ -44,43 +45,18 @@ export function App() {
             Lembre-se: você pode utilizar a variável <code className="text-primary">{'{transcription}'}</code> para adicionar o conteúdo da trancrição do vídeo selecionado.
           </p>
         </div>
-        
+
         <aside className="w-80 space-y-6">
-          <form className='space-y-6'>
-            <label
-              htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4"/>
-              Selecione um vídeo
-            </label>
-            <input type="file" id="video" accept="video/mp4" className="sr-only"/>
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed reasize-none"
-                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
-              />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Carregar vídeo
-              <Upload className="w-4 h-4 ml-2"/>
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
           <form className="space-y-3">
-          <div className="space-y-2">
+            <div className="space-y-2">
               <Label>Prompt</Label>
               <Select>
                 <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..."/>
+                  <SelectValue placeholder="Selecione um prompt..." />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="title">
@@ -93,7 +69,7 @@ export function App() {
               </Select>
             </div>
 
-            <Separator/>
+            <Separator />
 
             <div className="space-y-2">
               <Label>Modelo</Label>
@@ -130,7 +106,7 @@ export function App() {
 
             <Button type="submit" className="w-full">
               Executar
-              <Wand2 className=" w-4 h-4 ml-2"/>
+              <Wand2 className=" w-4 h-4 ml-2" />
             </Button>
           </form>
         </aside>
